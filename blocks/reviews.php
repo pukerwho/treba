a7cd11f2de92ddf528c8d1425758a05b78924a17
Vladimir Samojlov <v-moi@ya.ru>
2019-08-04 23:06:12 +0300

    edit text

--- blocks/reviews.php
+++ blocks/reviews.php
@@ -4,7 +4,7 @@
 			<div class="col-md-5">
 				<h2 class="treba-animate mb-5">Reviews</h2>
 				<div class="reviews__text treba-animate">
-					We try to do the work so that you want to leave a positive feedback.
+					We do our best in every desicion we make throughout the day to make you happy with final design and implementation.
 				</div>
 				<div class="reviews__quote">
 					<img src="<?php bloginfo('template_url') ?>/img/quote.svg" alt="">
@@ -16,7 +16,7 @@
 						<img src="<?php bloginfo('template_url') ?>/img/reviews1.png" alt="">
 					</div>
 					<div class="reviews__item-text mb-2">
-						I want to say thanks to the guys from Treba, especially Cyril. The work was done promptly, creatively, and most importantly, on time.
+						I want to say thanks to the guys from Treba, especially Kyrill. The work was done promptly, creatively, and most importantly, on time.
 					</div>
 					<div class="reviews__item-name">
 						Katerina Golitsyna
@@ -30,7 +30,7 @@
 						<img src="<?php bloginfo('template_url') ?>/img/reviews2.jpg" alt="">
 					</div>
 					<div class="reviews__item-text mb-2">
-						The team from Treba immediately understood the idea of the site and our vision. This greatly simplified the work. I note that it was easy and pleasant to work.
+						The team from Treba immediately understood the idea of the site and our vision. This greatly simplified the work. I noticed that it was easy and pleasant to work with them.
 					</div>
 					<div class="reviews__item-name">
 						Vladimir Lenskiy
@@ -44,13 +44,13 @@
 						<img src="<?php bloginfo('template_url') ?>/img/reviews3.png" alt="">
 					</div>
 					<div class="reviews__item-text mb-2">
-						We have been working for several months - I am completely satisfied with the result. The site is fire!
+						We have been working (with these guys) for several months - I am completely satisfied with the result. The site is a blast!
 					</div>
 					<div class="reviews__item-name">
 						Alexander
 					</div>
 					<div class="reviews__item-pos">
-						Director
+						Director WorldMoving
 					</div>
 				</div>
 			</div>
